503213fb59b5e397dc970eed1b1632724f6687d4
Shi Sha <emailtotheowner@gmail.com>
2020-03-11 10:34:01 -0400

    updated practice file-2nd version.

--- assets/test-2.js
+++ assets/test-2.js
@@ -19,57 +19,59 @@ var allQuestions = [   {
      
   {
     questionToAsk: "Which one of these uses server-side JavaScript?",
-      answerChoiceA: "Node.js",
-      answerChoiceB: "MongoDB",
-      answerChoiceC: "React",
-      answerChoiceD: "php.net",
+      answerChoiceA: "* " +  "Node.js",
+      answerChoiceB:  "* " +  "MongoDB",
+      answerChoiceC: "* " +  "React",
+      answerChoiceD: "* " + "php.net",
       rightAnswer: "A"
     },
     
 
   {
     questionToAsk: "Which dev tool feature you mostly use to check your code in Chrome browser?",
-      answerChoiceA: "Source",
-      answerChoiceB: "Elements",
-      answerChoiceC: "Application",
-      answerChoiceD: "Console",
+      answerChoiceA: "* " +  "Source",
+      answerChoiceB:  "* " +  "Elements",
+      answerChoiceC: "* " +  "Application",
+      answerChoiceD: "* " + "Console",
       rightAnswer: "D"
   },
 
   {
-    questionToAsk: "What would you do if you are struggling to code Javascript?",
-      answerChoiceA: "Ask the instructor",
-      answerChoiceB: "Search online",
-      answerChoiceC: "Use tutor help",
-      answerChoiceD: "All of the above",
+    questionToAsk: "What one of these keyword is used to declare a 'variable' in Javascript?",
+      answerChoiceA: "* " +  "array",
+      answerChoiceB:  "* " +  "method",
+      answerChoiceC: "* " +  "object",
+      answerChoiceD: "* " + "let",
       rightAnswer: "D"
     },
   
   {
     questionToAsk: "What would you do if you are struggling to code Javascript?",
-      answerChoiceA: "Ask the instructor",
-      answerChoiceB: "Search online",
-      answerChoiceC: "Use tutor help",
-      answerChoiceD: "All of the above",
+      answerChoiceA: "* " +  "Ask the instructor",
+      answerChoiceB:  "* " +  "Search online",
+      answerChoiceC: "* " +  "Use tutor help",
+      answerChoiceD: "* " + "All of the above",
       rightAnswer: "D"
     },
   ];
 
-  var lastQuestion = allQuestions.length -1;
-  var currentQuestion = 0;
-  var counterDisplay = 0;
-var timeForEachQuestion = 15;
-var timer = 0;
-scoreCounterEl = 0;
+var lastQuestion = allQuestions.length -1;
+var currentQuestion = 0;
+var counterDisplay = 75;
+// var timeForQuestions = 75;
+// var timeForEachQuestion = 15;  // 75 seconds / 5 question
+var TIMER;
+var actualScore = 0;
 
   // processing the question
-  function processQuestion () {
+function processQuestion () {
 var question = allQuestions[currentQuestion];
-questionFieldEl.textContent = question.questionToAsk;
-answerChoiceA.textContent = question.answerChoiceA;
-answerChoiceB.textContent = question.answerChoiceB;
-answerChoiceC.textContent = question.answerChoiceC;
-answerChoiceD.textContent = question.answerChoiceD;
+questionFieldEl.textContent = allQuestions.questionToAsk;
+answerChoiceA.textContent = allQuestions.answerChoiceA;
+answerChoiceB.textContent = allQuestions.answerChoiceB;
+answerChoiceC.textContent = allQuestions.answerChoiceC;
+answerChoiceD.textContent = allQuestions.answerChoiceD;
+timerCounterEl.textContent = counterDisplay;
   }
 
 startButtonEl.addEventListener("click", startQuiz);
@@ -77,33 +79,35 @@ startButtonEl.addEventListener("click", startQuiz);
 function startQuiz(){  
 processQuestion();
 updateCounter();
-timer = setInterval(counterDisplay, 1000);
+TIMER = setInterval(updateCounter, 1000);
+currentQuestion++;
+counterDisplay--;
  }
 
 
 // updating the counter
 function updateCounter() {
-  if (counterDisplay <= timeForEachQuestion) {
-    counterDisplay++;
-  }else{
+  if (counterDisplay == 75) {
+    counterDisplay--;
+  }
+  else{
     counterDisplay = 0;
   }
   if(processQuestion < lastQuestion){
     processQuestion++;
     processQuestion();
   
-  
   }else{
-    clearInterval(timer);
+    clearInterval(TIMER);
     updateScore();
   }
 }
 
 function checkAnswer(answer) {
-if(answer === allQuestion[processQuestion].rightAnswer) {
-  scoreCounterEl++;
+if(answer === allQuestions[processQuestion].rightAnswer) {
+  actualScore++;
+  scoreCounterEl.textContent = actualScore;
 }
-counterDisplay = 0;
 if(processQuestion < lastQuestion){
   processQuestion++;
   processQuestion();
@@ -115,9 +119,9 @@ if(processQuestion < lastQuestion){
 }
 }
 
-function updateScore() {
-var scorePoint = Math.floor(Math.random() * allQuestions.length);
-scoreCounterEl.textContent = scorePoint;
-}
+ function updateScore() {
+ var scorePoint = Math.floor(Math.random() * allQuestions.length);
+ scoreCounterEl.textContent = scorePoint;
+ }
 
 
