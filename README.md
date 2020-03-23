c171686f99a12cc6fec42c554deb38963dd02082
Shi Sha <emailtotheowner@gmail.com>
2020-03-23 14:52:02 -0400

    Modified codes to shorter version.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Code-Quiz-SS
 THIS IS A CODE QUIZ QUESTION-ANSWER JAVASCRIPT PROJECT.
+- First of all, I designed the UI layout without using Javascript to see what I needed.
+- I used css flex property and several rules to layout the design, which helped me visualize the final look.
+- Then taking the layout as a reference, I worked on Javascript to create HTML elements and style them. 
+- I then started further coding to make the app working. The code worked as intended, but the code was over 400 lines!!!! I wasn't happy what I did, and didn't have experience how to shorten the codes. Loops, functions, arrays, objects could be helpful, but did not have enough confident in me to work with them. So, I got help from tutor. The tutor showed me how could be done, then I did that way. I learned so many things about the Javascript by doing this homework. I am happy with the final result.
--- script.js
+++ script.js
@@ -1,260 +1,168 @@
 // Referencing/calling all related DOM elements
-var timerCounterEl = document.querySelector(".timerCounter");
-var questionFieldEl = document.getElementById("questionField");
-var answerFieldEl = document.getElementById("answerField"); 
 var startButtonEl = document.getElementById("startButton");
-var nextButtonOneEl = document.getElementById("nextButtonOne");
-var nextButtonTwoEl = document.getElementById("nextButtonTwo");
-var nextButtonThreeEl = document.getElementById("nextButtonThree");
-var nextButtonFourEl = document.getElementById("nextButtonFour");
-var nextButtonFiveEl = document.getElementById("nextButtonFive"); 
-var answerButtonsEl = document.getElementById("answerButton");
-var answerChoiceA = document.getElementById("answerChoiceOneA");
-var answerChoiceB = document.getElementById("answerChoiceOneB");
-var answerChoiceC = document.getElementById("answerChoiceOneC");
-var answerChoiceD = document.getElementById("answerChoiceOneD");
+var nextButtonEl = document.getElementById("nextButton");
+var questionWrapperEl = document.getElementById("questionWrapper");
+var questionFieldEl = document.getElementById("questionField");
+var answerButtonsEl = document.getElementById("answerButtons");
+var resetButtonEl = document.getElementById("resetButton");
+var eachButtonEl = document.getElementById("eachButton");
+var timerCounterEl = document.querySelector(".timerCounter");
 var scoreCounterEl = document.getElementById("scoreCounter");
 
-// Setting questions array - five questions
-var allQuestions = ["What is JavaScript?", 
-                    "Which is server-side JavaScript?",
-                    "A favorite Chrome dev tool to log code's output.",
-                    "A keyword to declare a 'variable' that is mostly similar to var.",
-                    "A popular Javascript library."
-                  ]
- 
-// Setting array for answer choices
-var answerChoiceOne = [
-"Scripting language", "MongoDB", "Source",  "array", "jQuery"
-]
-var answerChoiceTwo = [
-"Document editor", "Node.js", "Elements", "method", "Python"
-]
-var answerChoiceThree = [
-"Graphic design tool", "React", "Console", "object", "Swift"
-]
-var answerChoiceFour = [
-"Web browser", "php.net", "Application", "let", "Java"
-];
-// Setting right and wrong answer choices
-var rightAnswer = ["Right", "Right", "Right", "Right", "Right"];
-var wrongAnswer = ["Wrong", "Wrong", "Wrong", "Wrong", "Wrong"];
-var actualScore = 0; // user's score initialized to 0.
-var counterDisplay = 75; // timer set to 75 seconds total.
+var randomizedQuestions;
+var displayingQuestionIndex = 0;
+var score = 0; // user's score initialized to 0.
+var timerDisplay = 75; // timer set to 75 seconds total.
 var TIMER;
-// adding event listener to all buttons
-startButtonEl.addEventListener("click", startQuiz); // quiz starts
-nextButtonOneEl.addEventListener("click", newButtonOne); // takes to question 2
-nextButtonTwoEl.addEventListener("click", newButtonTwo); // takes to question 3
-nextButtonThreeEl.addEventListener("click", newButtonThree); // takes to question 4
-nextButtonFourEl.addEventListener("click", newButtonFour); // takes to question 5
-nextButtonFiveEl.addEventListener("click", newButtonFive); // takes to reset area
-nextButtonOneEl.style.display = ("none"); // initially hiding "Next" button-1
+
+startButtonEl.addEventListener("click", startQuiz);
+nextButtonEl.addEventListener('click', () => { 
+displayingQuestionIndex++;
+initializeAnotherQuestion();
+// if(allQuestions.question.length >= 5) {
+//   nextButtonEl.style.display = 'none';
+//   startButtonEl.style.display = 'none';
+// }
+})
 
 function startQuiz(){  
-questionFieldEl.textContent = allQuestions[0];
-answerChoiceA.textContent = answerChoiceOne[0];
-answerChoiceB.textContent = answerChoiceTwo[0];
-answerChoiceC.textContent = answerChoiceThree[0];
-answerChoiceD.textContent = answerChoiceFour[0];
-answerButtonsEl.style.display = ("block");
-startButtonEl.style.display = ("none");
+console.log("Try!");
+randomizedQuestions = allQuestions.sort(() => Math.random() * 1);
+displayingQuestionIndex = 0;
+initializeAnotherQuestion();
+// resetButtonEl.style.display = 'none';
+eachButtonEl.style.display = 'none';
 TIMER = setInterval(updateCounter, 1000);
-
-nextButtonOneEl.style.display = ("block"); // displaying "Next" button-1
-
- }
-
- // Click handlers for question 1
-function questionOneRightChoice() {
-  answerFieldEl.textContent = rightAnswer[0];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  actualScore++;
-  scoreCounterEl.textContent = actualScore;
-  // if(answerChoiceA[0]) {
-  // updateScore(totalScore);
-  // }
-   };
-
- function questionOneWrongChoice() {
-  answerFieldEl.textContent = wrongAnswer[0];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
+  }
   
-};
-function newButtonOne() {
-  questionFieldEl.textContent = allQuestions[1];
-  answerChoiceA.textContent = answerChoiceOne[1];
-answerChoiceB.textContent = answerChoiceTwo[1];
-answerChoiceC.textContent = answerChoiceThree[1];
-answerChoiceD.textContent = answerChoiceFour[1];
-answerFieldEl.textContent = "";
-nextButtonOneEl.style.display = ("none"); // hiding "Next" button-1
-  nextButtonTwoEl.style.display = ("block");  // displaying "Next" button-2
-};
-
-// Click handlers for question 2
-function questionTwoRightChoice() {
-  answerFieldEl.textContent = rightAnswer[1];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
-  
-};
-function questionTwoWrongChoice() {
-  answerFieldEl.textContent = wrongAnswer[1];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- 
-
-};
-function newButtonTwo() {
-  questionFieldEl.textContent = allQuestions[2];
-  answerChoiceA.textContent = answerChoiceOne[2];
-answerChoiceB.textContent = answerChoiceTwo[2];
-answerChoiceC.textContent = answerChoiceThree[2];
-answerChoiceD.textContent = answerChoiceFour[2];
-answerFieldEl.textContent = "";
-startButtonEl.style.display = ("none");
- nextButtonTwoEl.style.display = ("none"); // hiding "Next" button-2
- nextButtonThreeEl.style.display = ("block"); // displaying "Next" button-3
-
-};
-
-// Click handlers for question 3
-function questionThreeRightChoice() {
-  answerFieldEl.textContent = rightAnswer[2];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- 
-}
-function questionThreeWrongChoice() {
-  answerFieldEl.textContent = wrongAnswer[2];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- 
-
-}
-function newButtonThree() {
-  questionFieldEl.textContent = allQuestions[3];
-  answerChoiceA.textContent = answerChoiceOne[3];
-answerChoiceB.textContent = answerChoiceTwo[3];
-answerChoiceC.textContent = answerChoiceThree[3];
-answerChoiceD.textContent = answerChoiceFour[3];
-answerFieldEl.textContent = "";
-startButtonEl.style.display = ("none");
-nextButtonThreeEl.style.display = ("none"); // hiding "Next" button-3
-nextButtonFourEl.style.display = ("block"); // displaying "Next" button-4
-
-}
-// Click handlers for question 4
-function questionFourRightChoice() {
-  answerFieldEl.textContent = rightAnswer[3];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- 
-  
-
-}
-function questionFourWrongChoice() {
-  answerFieldEl.textContent = wrongAnswer[3];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- 
-}
-function newButtonFour() {
-  questionFieldEl.textContent = allQuestions[4];
-  answerChoiceA.textContent = answerChoiceOne[4];
-answerChoiceB.textContent = answerChoiceTwo[4];
-answerChoiceC.textContent = answerChoiceThree[4];
-answerChoiceD.textContent = answerChoiceFour[4];
-answerFieldEl.textContent = "";
-startButtonEl.style.display = ("none");
-nextButtonFourEl.style.display = ("none"); // hiding "Next" button-4
-nextButtonFiveEl.style.display = ("block"); // displaying "Next" button-5
-}
-
-// Click handlers for question 5
-function questionFiveRightChoice() {
-  answerFieldEl.textContent = rightAnswer[4];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
-  
-}
-function questionFiveWrongChoice() {
-  answerFieldEl.textContent = wrongAnswer[4];
-  answerChoiceA.textContent ="";
-  answerChoiceB.textContent ="";
-  answerChoiceC.textContent ="";
-  answerChoiceD.textContent ="";
-  scoreCounterEl.textContent = actualScore++;
- }
-
- function newButtonFive() {
-  questionFieldEl.textContent = "";
-  answerChoiceA.textContent = "";
-answerChoiceB.textContent = "";
-answerChoiceC.textContent = "";
-answerChoiceD.textContent = "";
-answerFieldEl.textContent = "";
-startButtonEl.style.display = ("none");
-nextButtonFourEl.style.display = ("none"); // hiding "Next" button-4
-nextButtonFiveEl.style.display = ("none"); // hiding "Next" button-5
-// resetButtonEl.style.display = ("block");
+function initializeAnotherQuestion() {
 resetQuiz();
+displayQuestion(randomizedQuestions[displayingQuestionIndex]);
 }
 
+function displayQuestion(question) {
+  questionFieldEl.innerText = question.question;
+  question.answerChoices.forEach(answer => {
+    var button = document.createElement('button');
+    button.innerText = answer.text;
+ button.classList.add('eachButton');
+  if(answer.rightAnswer) {
+  button.dataset.rightAnswer = answer.rightAnswer;
+  }
+ button.addEventListener("click", chooseAnswer);
+ answerButtonsEl.appendChild(button);
+ 
+  })
+}
+
+resetButtonEl.addEventListener("click", resetQuiz);
 function resetQuiz() {
+  clearStatusClass(document.body);
+//nextButtonEl.style.display = 'none';
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+  }
   
-location.reload();        
-
 }
 
-// update timer counter
+function chooseAnswer(e) {
+  var targetedButton = e.target;
+  var rightAnswer = targetedButton.dataset.rightAnswer
+setStatusClass(document.body, rightAnswer);
+Array.from(answerButtonsEl.children).forEach(button => {
+  setStatusClass(button, button.dataset.rightAnswer)
+})
+if (randomizedQuestions.length > displayingQuestionIndex + 1) {
+  // nextButtonEl.style.display = 'none';
+} else {
+  resetButtonEl.style.display = 'block';
+}
+}
+
+function setStatusClass(element, rightAnswer) {
+  clearStatusClass(element)
+ 
+}
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 function updateCounter() {
  
-    timerCounterEl.textContent = counterDisplay;
-    if(counterDisplay <= 0){
-      clearInterval(TIMER);
-     resetQuiz();
+  timerCounterEl.textContent = timerDisplay;
+  if(timerDisplay <= 0){
+    clearInterval(TIMER);
+   resetQuiz();
 }else{
-  counterDisplay--;
-  }
-  }
-  function updateScore(totalScore) {
-    var scorePoint = Math.floor(Math.random() * allQuestions.length);
-    scoreCounterEl.textContent = scorePoint;
-    actualScore++;
-    }
-//  function updateScore {
-//    if()
-//   scoreCounterEl.innerHTML = actualScore;
-//  }
+  timerDisplay--;
+}
+}
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// Setting questions array - five questions
+var allQuestions = [
+  {question: "What is JavaScript?",
+  answerChoices: [
+    {text: "Scripting language", rightAnswer: true},
+    {text: "Web browser", rightAnswer: false},
+    {text: "Document editor", rightAnswer: false},
+    {text: "Graphic design tool", rightAnswer: false}
+
+  ]
+}, 
+
+ { question:  "Which is server-side JavaScript?",
+  answerChoices: [
+    {text: "React", rightAnswer: false},
+    {text: "Node.js", rightAnswer: true},
+    {text: "php.net", rightAnswer: false},
+    {text: "MongoDB", rightAnswer: false}
+
+  ]
+},
+
+{ question: "A favorite Chrome dev tool to log code's output.",
+  answerChoices: [
+    {text: "Application", rightAnswer: false},
+    {text: "Source", rightAnswer: false},
+    {text: "Console", rightAnswer: true},
+    {text: "Elements", rightAnswer: false}
+
+  ]
+},
+
+{ question: "A keyword to declare a Javascript 'variable'.",
+  answerChoices: [
+    {text: "array", rightAnswer: false},
+    {text: "method", rightAnswer: false},
+    {text: "object", rightAnswer: false},
+    {text: "let", rightAnswer: true}
+
+  ]
+},
+
+{ question:  "A popular Javascript library.", 
+  answerChoices: [
+    {text: "jQuery", rightAnswer: true},
+    {text: "Python", rightAnswer: false},
+    {text: "Swift", rightAnswer: false},
+    {text: "Java", rightAnswer: false}
+
+  ]
+},
+
+
+
+
+
+
+
+
+
+]
+
 
  
 
